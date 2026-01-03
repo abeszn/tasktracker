2a1276523d4990e2b4233fbd0ddddb1f5963e10a
Nifemi Abe <deegamer318@gmail.com>
2026-01-03 14:46:39 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,47 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tasktracker/pages/splash_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:tasktracker/state/theme_notifier.dart';
+import 'package:tasktracker/utils/network.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   await Supabase.initialize(
     url: 'https://tqjxlbquonanziuiambr.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxanhsYnF1b25hbnppdWlhbWJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3MTcyMzgsImV4cCI6MjA3MjI5MzIzOH0.45-sgVsP8MBFYngSu66I4BYGrp1Ay4JNjrD4gTDfSq8',
   );
-  
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _checkInternet();
+  }
+
+  Future<void> _checkInternet() async {
+    final hasInternet = await hasRealInternet();
+
+    if (!hasInternet && mounted) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('No internet connection'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,17 +55,26 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
             useMaterial3: true,
           );
+
           final baseDark = ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange, brightness: Brightness.dark),
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.deepOrange,
+              brightness: Brightness.dark,
+            ),
             useMaterial3: true,
           );
+
           return MaterialApp(
             title: 'Task Tracker',
             theme: baseLight.copyWith(
-              snackBarTheme: const SnackBarThemeData(behavior: SnackBarBehavior.floating),
+              snackBarTheme: const SnackBarThemeData(
+                behavior: SnackBarBehavior.floating,
+              ),
             ),
             darkTheme: baseDark.copyWith(
-              snackBarTheme: const SnackBarThemeData(behavior: SnackBarBehavior.floating),
+              snackBarTheme: const SnackBarThemeData(
+                behavior: SnackBarBehavior.floating,
+              ),
             ),
             themeMode: settings.themeMode,
             debugShowCheckedModeBanner: false,
